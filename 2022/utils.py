f4a70884e8fc5f34fb211d66ba2d18bc4971aac0
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2022-12-09 19:36:44 +0100

    update utils

--- 2022/utils.py
+++ 2022/utils.py
@@ -1,2 +1,27 @@
 def lmap(*args, **kwargs):
     return list(map(*args, **kwargs))
+
+
+def flatten(nested_list):
+    flat = []
+    for el in nested_list:
+        try:
+            flat.extend(flatten(el))
+        except TypeError:
+            flat.append(el)
+    return flat
+
+
+def crange(c):
+    x, y = c
+    for i in range(x):
+        for j in range(y):
+            yield (i, j)
+
+
+def sum(acc, x):
+    return acc + x
+
+
+def mult(acc, x):
+    return acc * x
