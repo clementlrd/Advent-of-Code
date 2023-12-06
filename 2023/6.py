315000a0c1c47f38cac20efafb1e5bd7cf2d2a96
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-12-06 01:05:57 -0500

    day 6
    use polynomials & format

--- 2023/6.py
+++ 2023/6.py
@@ -1,21 +1,18 @@
 """Resolve a daily problem"""  # pylint: disable=invalid-name
-from typing import Iterable, Callable, Optional, Any
-# pylint: disable-next=unused-import
-from dataclasses import dataclass, field
+from typing import Iterable, Any
+from dataclasses import dataclass
 import os
-import __main__
-# import numpy as np
+import math
+from numpy.polynomial import Polynomial
 from utils import lines_of_file, lfilter, lmap
-from functools import reduce
 
 DATA_PATH = "inputs/"
 DAY = os.path.basename(__file__).split(".")[0]
-PART = 2
-TEST = False
 
 
 @dataclass
 class Boat:
+    """A boat with a loading time button."""
     loading_time: int
 
     @property
@@ -30,52 +27,59 @@ class Boat:
 
 @dataclass
 class Race:
+    """A race simulator to beat a record."""
     time_limit: int
     record: int
 
-    def nbr_beat_record(self) -> int:
+    def nbr_beat_record_naive(self) -> int:
+        """The number of race that beat the record. naive version."""
         return sum(
             Boat(loading_time=t).distance(self.time_limit) > self.record
             for t in range(0, self.time_limit - 1)
         )
 
+    def nbr_beat_record(self) -> int:
+        """The number of race that beat the record"""
+        t1, t2 = self.time_range_to_beat_record()
+        return t2 - t1 + 1
 
-def get_data() -> Iterable[list[int]]:
+    def time_range_to_beat_record(self) -> tuple[int, int]:
+        """Loading time that beat the record is solution of a polynomial equation.
+        More precisely, the loading time as to been between the roots of the polynomial."""
+        p = Polynomial([-self.record, self.time_limit, -1])
+        r1, r2 = p.roots()
+        return (math.ceil(r1), math.floor(r2))
+
+
+def get_data() -> Iterable[list[str]]:
     """Retrieve all the data to begin with."""
-    l = lines_of_file(f"{DATA_PATH}{DAY if not TEST else 'test'}.txt")
+    l = lines_of_file(f"{DATA_PATH}{DAY}.txt")
     l = map(lambda x: lfilter(None, x.split(' ')[1:]), l)
-    l = map(lambda x: lmap(int, x), l)
-    return list(l)
+    return l
 
 
 def part_1() -> None:
     """Code for section 1"""
-    max_time, record = get_data()
-    races = map(lambda x: Race(x[0], x[1]), zip(max_time, record))
-    l = map(lambda x: x.nbr_beat_record(), races)
+    l = map(lambda x: lmap(int, x), get_data())  # convert data to int
 
-    print_answer(reduce(lambda x, y: x * y, l))
+    total = 1
+    for time_limit, record in zip(*l):
+        r = Race(time_limit=time_limit, record=record)
+        total *= r.nbr_beat_record()
+
+    print_answer(total, part=1)
 
 
 def part_2() -> None:
     """Code for section 2"""
-    max_time, record = get_data()
-    max_time = int(reduce(lambda x, y: x + y, map(str, max_time)))
-    record = int(reduce(lambda x, y: x + y, map(str, record)))
-    race = Race(max_time, record)
+    l = map(''.join, get_data())    # data needs in fact to be concatenated
+    max_time, record = map(int, l)  # convert to int
+    race = Race(max_time, record)   # create the race
 
-    print_answer(race.nbr_beat_record())
-
-
-def main(fn: Optional[Callable[[], None]] = None) -> None:
-    """Main process"""
-    if fn is not None:
-        return fn()
-    ex = vars(__main__)[f"part_{PART}"]
-    return ex()
+    print_answer(race.nbr_beat_record(), part=2)
 
 
-def print_answer(answer: Any, part=PART, print_fn=print) -> None:
+def print_answer(answer: Any, part, print_fn=print) -> None:
     """Shorthand to print answer."""
     print("=" * 50)
     print(f"[DAY {DAY}] Answer to part {part} is:\n\n\t")
@@ -84,4 +88,5 @@ def print_answer(answer: Any, part=PART, print_fn=print) -> None:
 
 
 if __name__ == "__main__":
-    main()
+    part_1()  # P1: 840336
+    part_2()  # P2: 41382569
