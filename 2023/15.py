313da7096aea6aaa08704bf71d940eca421f3900
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-12-31 22:53:49 +0100

    day 15 rework

--- 2023/15.py
+++ 2023/15.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Iterator
 from dataclasses import dataclass, field
 from functools import reduce
-from utils import section, find_element, compose
+from utils import section, compose
 
 
 @dataclass
@@ -18,22 +18,28 @@ class Box:
     """Represents a box as available slots."""
     slots: list[Lens] = field(init=False, default_factory=list)
 
+    @property
+    def power(self) -> int:
+        """Compute the power of the box."""
+        return sum((slot + 1) * l.focal_length for slot, l in enumerate(self.slots))
+
     def add_lens(self, lens: Lens) -> None:
         """Add a lens to the box on top of the other slots.
         If a lens with the same label already exists, it is replaced."""
-        i, e = find_element(self.slots, lambda s: s.label == lens.label)
-        if e is None:
-            self.slots.append(lens)
-        else:
-            self.slots[i] = lens
+        for i, s in enumerate(self.slots):
+            if s.label == lens.label:
+                self.slots[i] = lens
+                return
+        self.slots.append(lens)
 
     def remove_lens(self, label: str) -> Lens | None:
         """Remove a lens from the box.
         When a lens is retrieved, slots are updated to fill in the gaps.
         """
-        i, e = find_element(self.slots, lambda s: s.label == label)
-        # if the element is not inside the box, return None
-        return e if e is None else self.slots.pop(i)
+        for i, s in enumerate(self.slots):
+            if s.label == label:
+                return self.slots.pop(i)
+        return None
 
 
 def hash_str(string: str) -> int:
@@ -45,12 +51,10 @@ def execute_procedure(procedures: list[str]) -> list[Box]:
     """Execute a serie of commands according to HASHMAP procedure."""
     boxes = [Box() for _ in range(256)]
     for cmd in procedures:
-        if '-' in cmd:
-            # retrieve a Lens with a given label from its box
+        if '-' in cmd:    # retrieve a Lens with a given label from its box
             label = cmd[:-1]
             boxes[hash_str(label)].remove_lens(label)
-        elif "=" in cmd:
-            # add a lens to a special box
+        elif "=" in cmd:  # add a lens to a special box
             label, value = cmd.split("=")
             boxes[hash_str(label)].add_lens(Lens(label, focal_length=int(value)))
         else:
@@ -60,11 +64,7 @@ def execute_procedure(procedures: list[str]) -> list[Box]:
 
 def focusing_power(boxes: list[Box]) -> int:
     """Compute focusing power metric."""
-    # TODO: move inside box
-    def box_power(box: Box) -> int:
-        return sum((slot + 1) * l.focal_length for slot, l in enumerate(box.slots))
-
-    return sum((i + 1) * box_power(box) for i, box in enumerate(boxes))
+    return sum((i + 1) * box.power for i, box in enumerate(boxes))
 
 
 @section(year=2023, day=15, part=1, sol=509784)
