c42a2a7254bca9bbb1187ce7229a6f9904cfaefb
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-12-31 15:18:02 +0100

    day 9 rework

--- 2023/9.py
+++ 2023/9.py
@@ -1,61 +1,55 @@
 """Resolve a daily problem"""  # pylint: disable=invalid-name
 from __future__ import annotations
 from typing import Iterator
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from itertools import pairwise, starmap
+from functools import reduce
+from operator import add
+
 from utils import section, lmap
 
 
-@dataclass(slots=True)
+@dataclass(frozen=True, slots=True)
 class History:
     """A record of an history entry"""
     values: list[int]
-    differences: list[list[int]] = field(init=False)
 
-    def __post_init__(self) -> None:
-        self.forward()
-
-    def forward(self) -> None:
+    def forward(self) -> list[list[int]]:
         """Compute all the differences"""
-        self.differences = [self.values]
-        while any(self.differences[-1]):
-            new_diff = starmap(lambda x, y: y - x, pairwise(self.differences[-1]))
-            self.differences.append(list(new_diff))
+        differences = [self.values]
+        while any(differences[-1]):
+            # add a new layer of differences
+            new_diff = starmap(lambda x, y: y - x, pairwise(differences[-1]))
+            differences.append(list(new_diff))
+        return differences
 
     def predict_next(self) -> int:
         """Extrapolate the next value"""
-        last_values = (diffs[-1] for diffs in self.differences[::-1])
-        predicted = 0
-        for value in last_values:
-            predicted = predicted + value
-        return predicted
+        last_values = (diffs[-1] for diffs in self.forward()[::-1])
+        return reduce(add, last_values)
 
     def predict_before(self) -> int:
         """Extrapolate the first value"""
-        # TODO: use predict next with reversed order
-        first_values = (diffs[0] for diffs in self.differences[::-1])
-        predicted = 0
-        for value in first_values:
-            predicted = value - predicted
-        return predicted
+        first_values = (diffs[0] for diffs in self.forward()[::-1])
+        return reduce(lambda pred, v: v - pred, first_values)
 
-    @staticmethod
-    def from_str(string: str) -> History:
+    @classmethod
+    def from_repr(cls, _repr: str) -> History:
         """Create an History entry from a raw string"""
-        return History(lmap(int, string.split(" ")))
+        return cls(lmap(int, _repr.split(" ")))
 
 
 @section(year=2023, day=9, part=1, sol=2008960228)
 def part_1(data: Iterator[str]) -> int:
     """Code for section 1"""
-    histories = map(History.from_str, data)
+    histories = map(History.from_repr, data)
     return sum(x.predict_next() for x in histories)
 
 
 @section(year=2023, day=9, part=2, sol=1097)
 def part_2(data: Iterator[str]) -> int:
     """Code for section 2"""
-    histories = map(History.from_str, data)
+    histories = map(History.from_repr, data)
     return sum(x.predict_before() for x in histories)
 
 
