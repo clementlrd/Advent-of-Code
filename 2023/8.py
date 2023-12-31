39670f195c6e6b8122b17677d01e09b3293e97c6
clément LAROUDIE <clement.laroudie@ensta-paris.fr>
2023-12-31 14:56:02 +0100

    day 8 refactor

--- 2023/8.py
+++ 2023/8.py
@@ -1,33 +1,39 @@
 """Resolve a daily problem"""  # pylint: disable=invalid-name
 from __future__ import annotations
+from typing import Iterator, Iterable
 import math
-from typing import Iterator
 from itertools import cycle
-from utils import section, lmap
+from dataclasses import dataclass
+from functools import partial
+
+from utils import section
 
 
+@dataclass(frozen=True, slots=True)
 class Node:
     """Contains a node of the map."""
-
-    def __init__(self, row: str) -> None:
-        self.name, raw_LR = row.split(" = ")
-        self.L, self.R = raw_LR[1:-1].split(', ')
-
-    def build_path(self, dictionnary: dict[str, int], nodes: list[Node]) -> None:
-        """Construct the relation between nodes"""
-        if isinstance(self.R, str):
-            self.R = nodes[dictionnary[self.R]]
-        if isinstance(self.L, str):
-            self.L = nodes[dictionnary[self.L]]
+    L: str
+    R: str
 
 
-def rollout(seq: str, node: Node, part=1) -> int:
-    """Return the number of steps to reach an end"""
-    for steps, d in enumerate(cycle(seq)):
-        if part == 1 and node.name == "ZZZ" or part == 2 and node.name[-1] == 'Z':
-            return steps
-        node = getattr(node, d)
-    return -1
+class Tree:
+    """Represents the binary Tree"""
+
+    def __init__(self, nodes_repr: Iterable[str]) -> None:
+        self.nodes = dict[str, Node]()
+        for node in nodes_repr:
+            name, childs = node.split(' = ')
+            L, R = childs[1:-1].split(', ')
+            self.nodes[name] = Node(L, R)
+
+    def rollout(self, seq: str, start: str, part=1) -> int:
+        """Return the number of steps to reach an end"""
+        node_name = start
+        for steps, d in enumerate(cycle(seq)):
+            if part == 1 and node_name == "ZZZ" or part == 2 and node_name[-1] == 'Z':
+                return steps
+            node_name: str = getattr(self.nodes[node_name], d)
+        return -1
 
 
 @section(year=2023, day=8, part=1, sol=13019)
@@ -35,13 +41,7 @@ def part_1(data: Iterator[str]) -> int:
     """Code for section 1"""
     sequence = next(data)
     next(data)  # empty line
-    nodes = lmap(Node, data)
-
-    name_to_id = {node.name: i for i, node in enumerate(nodes)}
-    for node in nodes:
-        node.build_path(name_to_id, nodes)
-
-    return rollout(seq=sequence, node=nodes[0])
+    return Tree(data).rollout(seq=sequence, start="AAA")
 
 
 @section(year=2023, day=8, part=2, sol=13524038372771)
@@ -49,16 +49,12 @@ def part_2(data: Iterator[str]) -> int:
     """Code for section 2"""
     sequence = next(data)
     next(data)  # empty line
-    nodes = lmap(Node, data)
 
-    name_to_id = {node.name: i for i, node in enumerate(nodes)}
-    for node in nodes:
-        node.build_path(name_to_id, nodes)
+    tree = Tree(data)
+    start_nodes = filter(lambda x: x[-1] == 'A', tree.nodes.keys())
+    count_steps = partial(tree.rollout, sequence, part=2)
 
-    start_nodes = set(filter(lambda x: x.name[-1] == 'A', nodes))
-    steps = map(lambda n: rollout(seq=sequence, node=n, part=2), start_nodes)
-
-    return math.lcm(*steps)
+    return math.lcm(*map(count_steps, start_nodes))
 
 
 if __name__ == "__main__":
