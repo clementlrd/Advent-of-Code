e11e4df06f631474ccddddb5f2efbf8ad11b9da3
Clément Laroudie <clem.laroudie@gmail.com>
2024-12-06 23:56:37 +0100

    2nd tentative day 6

--- 2024/6.py
+++ 2024/6.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterator
 from copy import deepcopy
+from tqdm import tqdm
 from aoc import section, load_input
 from aoc.grid import Grid
 from aoc.utils import Coordinate, Direction, Neighborhood
@@ -7,6 +8,7 @@ from aoc.utils import Coordinate, Direction, Neighborhood
 N, S, E, W = Neighborhood('c4')
 TURN = {N: E, E: S, S: W, W: N}
 REPR = {N: "^", S: "v", E: ">", W: '<'}
+DIR = {v: k for k,v in REPR.items()}
 
 class Loop(Exception): ...
 
@@ -17,50 +19,69 @@ def steps(data: Grid, pos: tuple[int, int], d: Direction):
         yield curr_pos, data[curr_pos]
 
 
-def forward(grid: Grid[str], start: tuple[int, int], d: Direction) -> None:
-    for curr_pos, e in steps(grid, start, d):
-        if e == "#":
-            previous = Coordinate(curr_pos) - d
-            grid[previous] = REPR[TURN[d]]
-            #x,y = curr_pos
-            #print(grid.subgrid((x-2, y-2), (x+3, y+3)))
-            return forward(grid, previous, TURN[d])
-        if grid[curr_pos] == REPR[d]:
-            raise Loop()
-        if grid[curr_pos] == REPR[TURN[d]] and grid[Coordinate(curr_pos) + d] in "#O":
-            raise Loop()
-        #if grid[curr_pos] == REPR[-d] and grid[Coordinate(curr_pos) + d] in "#O" and grid[Coordinate(curr_pos) + TURN[d]] in "#O" and grid[Coordinate(curr_pos) - TURN[d]] == REPR[TURN[d]]:
-        if grid[curr_pos] == REPR[-d] and grid[Coordinate(curr_pos) + d] in "#O" and ( grid[Coordinate(curr_pos) + TURN[d]] in "#O" or  grid[Coordinate(curr_pos) - TURN[d]] in "#O"):
-            raise Loop()
-        grid[curr_pos] = REPR[d]
-    # reached a border of the grid
+# def forward(grid: Grid[str], start: tuple[int, int], d: Direction) -> None:
+#     for curr_pos, e in steps(grid, start, d):
+#         if e == "#":
+#             previous = Coordinate(curr_pos) - d
+#             #grid[previous] = REPR[TURN[d]]
+#             return forward(grid, previous, TURN[d])
+#         grid[curr_pos] = REPR[d]
+#     # reached a border of the grid
 
-def find_loops(grid: Grid[str], start: tuple[int, int], d: Direction) -> Iterator[tuple[int, int]]:
-    for curr_pos, e in steps(grid, start, d):
-        assert e != "O"
-        if e == "#":
-            yield from find_loops(grid, Coordinate(curr_pos) - d, TURN[d])
-            return
-        # try putting an obstacle, do we create a loop ?
-        obstacle = Coordinate(curr_pos) + d
-        if not grid.is_valid(obstacle):
+# def forward(grid: Grid[str], start: tuple[int, int], d: Direction) -> None:
+#     pos = start
+#     while True:
+#         for curr_pos, e in steps(grid, pos, d):
+#             if e == "#":
+#                 pos , d = Coordinate(curr_pos) - d, TURN[d]
+#                 break
+#             else:
+#                 grid[curr_pos] = REPR[d]
+#         else:
+#             return  # reached a border of the grid
+
+def forward(grid: Grid[str], start: tuple[int, int], d: Direction, write=True) -> None:
+    pos = start
+    seen = set()
+    while True:
+        #print("here")
+        for curr_pos, e in steps(grid, pos, d):
+            if (h := (*curr_pos, *d)) in seen:
+                raise Loop()
+            if e in "#O":
+                seen.add(h)
+                pos , d = Coordinate(curr_pos) - d, TURN[d]
+                break
+            elif write:
+                grid[curr_pos] = REPR[d]
+        else:
             return # reached a border of the grid
-        if grid[obstacle] != "#": # make sur there is no natural obstacle
+
+def find_loops(grid: Grid[str], start: tuple[int, int], d: Direction) -> Iterator:
+    pos = start
+    while True:
+        for curr_pos, e in steps(grid, pos, d):
+            if e == "#":
+                pos , d = Coordinate(curr_pos) - d, TURN[d]
+                break
+
+            # try putting obstacle
+            if not grid.is_valid(obstacle := Coordinate(curr_pos) + d):
+                return # reached a border of the grid
+            if grid[obstacle] == "#": # natural obstacle, no loop
+                continue
+
             try:
-                if grid[curr_pos] == REPR[TURN[d]]:
-                    raise Loop() # already been there 
-                temp_grid = deepcopy(grid)
-                temp_grid[curr_pos] = REPR[TURN[d]]
-                temp_grid[obstacle] = "O"
-                forward(temp_grid, curr_pos, TURN[d])
-            except (Loop, RecursionError):
-                yield obstacle
+                #temp_grid = deepcopy(grid)
+                grid[obstacle] = "O"
+                forward(grid, curr_pos, TURN[d], write=False)
+            except Loop:
+                yield (*obstacle, *d)
+            finally:
+                grid[obstacle] = "."
 
-        if grid[curr_pos] == REPR[TURN[d]] and grid[Coordinate(curr_pos) + d] == "#":
-            raise Loop()
-
-        # continue path
-        grid[curr_pos] = REPR[d]
+            # continue path
+            grid[curr_pos] = REPR[d]
 
 
 @section.p1(sol=4722)
@@ -72,16 +93,14 @@ def part_1() -> int:
     print(str(data).replace(" ", ""))
     return sum(c in REPR.values() for _, c in data.enumerate())
 
-# 1661, 1674
+# 1661, 1674, 1623, 1727
 # 846 < 1807
 @section.p2()
 def part_2() -> int:
     """Code for section 2"""
     data = Grid([list(r) for r in load_input()])
     start = next((p for p, e in data.enumerate() if e == "^"))
-    a = len(set(find_loops(data, start, N)))
-    print(str(data).replace(" ", ""))
-    return a
+    return len(set(find_loops(data, start, N)))
 
 if __name__ == "__main__":
     part_1()
