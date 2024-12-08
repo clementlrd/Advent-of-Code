ef35ee6b487f421fe9d53ca60b57804ee4214c96
Clément Laroudie <clem.laroudie@gmail.com>
2024-12-08 23:38:25 +0100

    :fire: slightly improve day 8

--- 2024/8.py
+++ 2024/8.py
@@ -5,48 +5,42 @@ from aoc import section, load_input
 from aoc.grid import Grid
 from aoc.utils import Coordinate
 
-def parse(data: list[str]):
-    """Parse input into ..."""
-    ...
+def get_antenas(grid: Grid[str]) -> dict[str, list[Coordinate]]:
+    antennas = defaultdict(list)
+    for pos, c in grid.enumerate():
+        if c != ".":
+            antennas[c].append(Coordinate(pos))
+    return antennas
 
-@section.p1()
+def antenna_pairs(antennas):
+    return ((a, b) for positions in antennas.values() for a, b in combinations(positions, 2))
+
+
+@section.p1(sol=348)
 def part_1() -> int:
     """Code for section 1"""
     data = Grid.from_strings(load_input())
-    antenas: dict[str, list[Coordinate]] = defaultdict(list)
-    for pos, c in data.enumerate():
-        if c != ".":
-            antenas[c].append(Coordinate(pos))
-    for c, positions in antenas.items():
-        for a, b in combinations(positions, 2):
-            d = (a - b)
-            for p in ((a + d), (b - d)):
-                if data.is_valid(p):
-                    data[p] = "#"
-    return sum( c == "#" for _, c in data.enumerate() )
+    antennas = get_antenas(data)
+    for a, b in antenna_pairs(antennas):
+        d = (a - b)
+        for p in ((a + d), (b - d)):
+            if data.is_valid(p):
+                data[p] = "#"
+    return sum( c == "#" for c in data )
 
-@section.p2()
+
+@section.p2(sol=1221)
 def part_2() -> int:
     """Code for section 2"""
     data = Grid.from_strings(load_input())
-    antenas: dict[str, list[Coordinate]] = defaultdict(list)
-    for pos, c in data.enumerate():
-        if c != ".":
-            antenas[c].append(Coordinate(pos))
-    for c, positions in antenas.items():
-        for a, b in combinations(positions, 2):
-            d = (a - b)
+    antennas = get_antenas(data)
+    for a, b in antenna_pairs(antennas):
+        for fn in (lambda d: a + d, lambda d: b - d):
             k = 0
-            while data.is_valid(p := a + k * d):
+            while data.is_valid(p := fn(k * (a - b))):
                 data[p] = "#"
                 k += 1
-            k = 0
-            while data.is_valid(p := b - k * d):
-                data[p] = "#"
-                k += 1
-            
-    print(data)
-    return sum( c == "#" for _, c in data.enumerate() )
+    return sum( c == "#" for c in data )
 
 
 if __name__ == "__main__":
